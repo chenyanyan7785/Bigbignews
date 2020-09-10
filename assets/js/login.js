37f7d550c08c869ea76fca8277a83ee59c235caf
chenyanyan <13739277785@139.com>
2020-09-10 10:52:52 +0800

    发起注册和登录的Ajax请求

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,6 +12,8 @@ $(function() {
     // 自定义表单属性验证
     // 从layui中获取form元素
     var form = layui.form
+        // 从layui中获取
+    var layer = layui.layer
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [
@@ -26,5 +28,52 @@ $(function() {
             }
         }
 
-    })
+    });
+    // 发起注册用户的Ajax请求
+    // 监听注册表单的提交事件
+    $('#form-reg').on('submit', function(e) {
+        // 阻止默认行为
+        e.preventDefault()
+        var data = {
+                username: $('.reg-box [name=username]').val(),
+                password: $('.reg-box [name=password]').val()
+            }
+            //提交数据
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
+            console.log(res);
+            //注册失败
+            if (res.status == 1) {
+
+                return layer.msg(res.message);
+
+            }
+            layer.msg('注册成功');
+            //模仿手动操作点击事件
+            $('.link_login').click()
+
+        })
+    });
+    // 监听登录表单的提交事件
+    $("#form-login").on('submit', function(e) {
+        e.preventDefault(); //阻止默认行为
+        // 获取表单中的数据
+        var data = $(this).serialize()
+            //获取用户登录信息
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            type: 'post',
+            data: data,
+            success: function(res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登录失败');
+                }
+                layer.msg('登录成功');
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+
+            }
+        });
+    });
 })
